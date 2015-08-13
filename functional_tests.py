0b6826b18fe92205c21cae1ca0e2817c66b6f3f6
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-08-13 11:32:54 +0100

    Remove unnecessary whitespace in the definition of teardown

--- functional_tests.py
+++ functional_tests.py
@@ -1,34 +1,44 @@
 from selenium import webdriver
 import unittest
 
-browser = webdriver.Chrome('E:\\Program Files\\Google\\Chrome\\Application\\chromedriver.exe')
 
-# Edith has heard about a cool new online to-do app. She goes
-# to check out its homepage
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-# She notices the page title and header mention to-do lists
-assert 'To-Do' in browser.title, "Browser title was " + browser.title
+    def setUp(self):
+        self.browser = webdriver.Chrome('E:\\Program Files\\Google\\Chrome\\Application\\chromedriver.exe')
 
-# She is invited to enter a to-do item straight away
+    def tearDown(self):
+        self.browser.quit()
 
-# She types "Buy peacock feathers" into a text box (Edith's hobby
-# is tying fly-fishing lures)
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        browser.get('http://localhost:8000')
 
-# When she hits enter, the page updates, and now the page lists
-# "1: Buy peacock feathers" as an item in a to-do list
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', browser.title) 
+        self.fail('Finish the test!')
+        
+        # She is invited to enter a to-do item straight away
 
-# There is still a text box inviting her to add another item. She
-# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
 
-# The page updates again, and now shows both items on her list
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
 
-# Edith wonders whether the site will remember her list. Then she sees
-# that the site has generated a unique URL for her -- there is some
-# explanatory text to that effect.
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
 
-# She visits that URL - her to-do list is still there.
+        # The page updates again, and now shows both items on her list
 
-# Satisfied, she goes back to sleep
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
 
-browser.quit()
+        # She visits that URL - her to-do list is still there.
+
+        # Satisfied, she goes back to sleep
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
