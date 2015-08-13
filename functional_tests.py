8bf02d0aed9ecd59836f2906ba426e06d57559c3
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-08-13 10:57:22 +0100

    Write functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,12 @@
 from selenium import webdriver
 
 browser = webdriver.Chrome('E:\\Program Files\\Google\\Chrome\\Application\\chromedriver.exe')
+
+# Edith has heard about a cool new online to-do app. She goes
+# to check out its homepage
 browser.get('http://localhost:8000')
 
+# She notices the page title and header mention to-do lists
 assert 'Django' in browser.title
+
+#
